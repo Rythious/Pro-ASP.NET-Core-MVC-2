c8511204efc17b88bbb7f2cb93c21f9863d5bb06
Justin Morgan <Rythious@gmail.com>
2018-10-20 12:41:58 -0400

    Chapter 4. Bottom of page 102

--- Example Projects/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ Example Projects/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -9,24 +9,10 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        public async Task<ViewResult> Index()
         {
-            object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 };
-            decimal total = 0;
-            for (int i = 0; i < data.Length; i++)
-            {
-                switch (data[i])
-                {
-                    case decimal decimalValue:
-                        total += decimalValue;
-                        break;
-                    case int intValue when intValue > 50:
-                        total += intValue;
-                        break;
-                }
-            }
-
-            return View("Index", new string[] { $"Total: {total:C2}" });
+            long? length = await MyAsyncMethods.GetPageLength();
+            return View(new string[] { $"Length: {length}" });
         }
     }
 }
