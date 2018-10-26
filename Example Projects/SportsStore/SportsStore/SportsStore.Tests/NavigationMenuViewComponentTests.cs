6c036b087206e379357dee7dd695fa6c87a10e40
Justin Morgan <Rythious@gmail.com>
2018-10-26 07:32:16 -0400

    Chapter 9. Paused at page 250 before Highlighting the Current Category

---
+++ Example Projects/SportsStore/SportsStore/SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using SportsStore.Components;
+using SportsStore.Controllers;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class NavigationMenuViewComponentTests
+    {
+        [Fact]
+        public void Can_Select_Categories()
+        {
+            //Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductId = 1, Name = "P1", Category = "Apples"},
+                new Product {ProductId = 2, Name = "P2", Category = "Apples"},
+                new Product {ProductId = 3, Name = "P3", Category = "Plums"},
+                new Product {ProductId = 4, Name = "P4", Category = "Oranges"}
+            }).AsQueryable<Product>());
+
+            NavigationMenuViewComponent target = new NavigationMenuViewComponent(mock.Object);
+
+            //Act
+            string[] results = ((IEnumerable<string>)(target.Invoke() as ViewViewComponentResult).ViewData.Model).ToArray();
+
+            //Assert
+            Assert.True(Enumerable.SequenceEqual(new string[] { "Apples", "Oranges", "Plums" }, results));
+        }
+    }
+}
--- Example Projects/SportsStore/SportsStore/SportsStore.Tests/PageLinkTagHelperTests.cs
+++ Example Projects/SportsStore/SportsStore/SportsStore.Tests/PageLinkTagHelperTests.cs
@@ -12,7 +12,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-
 namespace SportsStore.Tests
 {
     public class PageLinkTagHelperTests
