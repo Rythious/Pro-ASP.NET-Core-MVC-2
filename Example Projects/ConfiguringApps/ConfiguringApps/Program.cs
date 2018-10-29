3ac040d8f19910c38505ec2f7ef22340fb4a11d0
Justin Morgan <Rythious@gmail.com>
2018-10-28 21:10:04 -0400

    Chapter 14. Paused at page 386

---
+++ Example Projects/ConfiguringApps/ConfiguringApps/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConfiguringApps
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>();
+        }
+    }
+}
