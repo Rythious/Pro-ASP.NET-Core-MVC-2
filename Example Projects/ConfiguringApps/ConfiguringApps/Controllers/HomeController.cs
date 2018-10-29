55c73b9a65005d9c03cfd3dbe3d59ad4194aa564
Justin Morgan <Rythious@gmail.com>
2018-10-28 22:44:30 -0400

    Chapter 14. Paused at page 400 before Understanding How the Configure...

--- Example Projects/ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
+++ Example Projects/ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConfiguringApps.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 namespace ConfiguringApps.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() => View(new Dictionary<string, string> { ["Message"] = "This is the Index action" });
+        private UptimeService uptime;
+
+        public HomeController(UptimeService up)
+        {
+            uptime = up;
+        }
+
+        public ViewResult Index() => View(new Dictionary<string, string>
+        {
+            ["Message"] = "This is the Index action",
+            ["Uptime"] = $"{uptime.Uptime}ms"
+        });
     }
 }
