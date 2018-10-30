aca0f16d05b48af9bfbba5ee53e4be75cbb784d3
Justin Morgan <Rythious@gmail.com>
2018-10-30 06:47:44 -0400

    Finished Chapter 15. Paused at page 469

--- Example Projects/UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
+++ Example Projects/UrlsAndRoutes/UrlsAndRoutes/Controllers/CustomerController.cs
@@ -7,6 +7,7 @@ using UrlsAndRoutes.Models;
 
 namespace UrlsAndRoutes.Controllers
 {
+    [Route("app/[controller]/actions/[action]/{id:weekday?}")]
     public class CustomerController : Controller
     {
         public ViewResult Index() => View("Result", new Result { Controller = nameof(CustomerController), Action = nameof(Index) });
--- Example Projects/UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
+++ Example Projects/UrlsAndRoutes/UrlsAndRoutes/Controllers/HomeController.cs
@@ -10,5 +10,17 @@ namespace UrlsAndRoutes.Controllers
     public class HomeController : Controller
     {
         public ViewResult Index() => View("Result", new Result { Controller = nameof(HomeController), Action = nameof(Index) });
+
+        public ViewResult CustomVariable(string id)
+        {
+            Result r = new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(CustomVariable)
+            };
+            r.Data["Id"] = id ?? "<no value>";
+            r.Data["catchall"] = RouteData.Values["catchall"];
+            return View("Result", r);
+        }
     }
 }
