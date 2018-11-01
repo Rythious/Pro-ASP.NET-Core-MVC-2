c27a04e09f156c70dd8a130e4d7782c20c697869
Justin Morgan <Rythious@gmail.com>
2018-11-01 07:29:26 -0400

    Finished Chapter 16. Paused at page 507

--- Example Projects/UrlsAndRoutes/UrlsAndRoutes/Startup.cs
+++ Example Projects/UrlsAndRoutes/UrlsAndRoutes/Startup.cs
@@ -18,7 +18,11 @@ namespace UrlsAndRoutes
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<RouteOptions>(options => options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint)));
+            services.Configure<RouteOptions>(options => {
+                options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint));
+                options.LowercaseUrls = true;
+                options.AppendTrailingSlash = true;
+                });
             services.AddMvc();
         }
 
@@ -29,8 +33,10 @@ namespace UrlsAndRoutes
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseMvc(routes => {
-                //routes.MapRoute(name: "NewRoute", template: "App/Do{action}", defaults: new { controller = "Home" });
+                routes.MapRoute(name: "areas", template: "{area:exists}/{controller=Home}/{action=Index}");
+                routes.Routes.Add(new LegacyRoute(app.ApplicationServices, "/articles/Windows_3.1_Overview.html", "/old/.NET_1.0_Class_Library"));
                 routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "out", template: "outbound/{controller=Home}/{action=Index}");
             });
         }
     }
