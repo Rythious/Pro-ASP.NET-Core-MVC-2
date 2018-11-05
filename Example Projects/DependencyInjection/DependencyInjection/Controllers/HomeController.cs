a643cc8d76500079d5fc60c55011a2591bfabdb7
Justin Morgan <Rythious@gmail.com>
2018-11-05 07:11:55 -0500

    Chapter 18. Paused at page 564

---
+++ Example Projects/DependencyInjection/DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using DependencyInjection.Infrastructure;
+using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IRepository Repository { get; } = TypeBroker.Repository;
+
+        public ViewResult Index() => View(Repository.Products);
+    }
+}
