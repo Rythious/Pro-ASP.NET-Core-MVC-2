7e31cc16a5499f7664d5bd2b8368a495bc6d7a71
Justin Morgan <Rythious@gmail.com>
2018-11-06 07:11:44 -0500

    Chapter 19. Paused at page 592 before Understanding Filters

---
+++ Example Projects/Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filters.Controllers
+{
+    [RequireHttps]
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View("Message", "This is the Index action on the Home controller");
+        }
+
+        public ViewResult SecondAction()
+        {
+            return View("Message", "This is the SecondAction action on the Home controller");
+        }
+    }
+}
