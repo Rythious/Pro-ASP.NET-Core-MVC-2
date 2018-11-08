3b115729637311d941f00e2b95c36b3d3382953b
Justin Morgan <Rythious@gmail.com>
2018-11-07 23:17:19 -0500

    Chapter 19. Paused at page 596

--- Example Projects/Filters/Filters/Controllers/HomeController.cs
+++ Example Projects/Filters/Filters/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Filters.Infrastructure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Filters.Controllers
 {
-    [RequireHttps]
+    [HttpsOnly]
     public class HomeController : Controller
     {
         public ViewResult Index()
