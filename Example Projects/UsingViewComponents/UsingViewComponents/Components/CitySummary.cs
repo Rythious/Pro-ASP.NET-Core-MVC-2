53a0060f3d21cade7bef601c3548618cf9e59686
Justin Morgan <Rythious@gmail.com>
2018-11-11 22:25:22 -0500

    Finished Chapter 22. Paused at page 721

---
+++ Example Projects/UsingViewComponents/UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System.Linq;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private ICityRepository repository;
+
+        public CitySummary(ICityRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke(bool showList)
+        {
+            if (showList)
+            {
+                return View("CityList", repository.Cities);
+            }
+            else
+            {
+                return View(new CityViewModel
+                {
+                    Cities = repository.Cities.Count(),
+                    Population = repository.Cities.Sum(c => c.Population)
+                });
+            }
+        }
+    }
+}
