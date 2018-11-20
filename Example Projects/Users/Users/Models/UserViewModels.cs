1a4d89431bc23aadb8d3cf7e8e6ada9e76e6b5a0
Justin Morgan <Rythious@gmail.com>
2018-11-19 21:33:43 -0500

    Finished Chapter 29. Paused at page 949

--- Example Projects/Users/Users/Models/UserViewModels.cs
+++ Example Projects/Users/Users/Models/UserViewModels.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Users.Models
 {
@@ -21,5 +23,21 @@ namespace Users.Models
         [Required]
         [UIHint("password")]
         public string Password { get; set; }
+    }
+
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<AppUser> Members { get; set; }
+        public IEnumerable<AppUser> NonMembers { get; set; }
+    }
+
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
     }
 }
--- Example Projects/Users/Users/Startup.cs
+++ Example Projects/Users/Users/Startup.cs
@@ -46,6 +46,7 @@ namespace Users
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseMvcWithDefaultRoute();
+            AppIdentityDbContext.CreateAdminAccount(app.ApplicationServices, Configuration).Wait();
         }
     }
 }
