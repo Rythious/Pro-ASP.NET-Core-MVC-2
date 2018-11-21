1aab1014aeacf25164e70766ec429112d38ca9b5
Justin Morgan <Rythious@gmail.com>
2018-11-20 20:08:08 -0500

    Chapter 31. Paused at page 992 before Creating a Model Convention

---
+++ Example Projects/ConventionsAndConstraints/ConventionsAndConstraints/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using ConventionsAndConstraints.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConventionsAndConstraints.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("Result", new Result { Controller = nameof(HomeController), Action = nameof(Index) });
+
+        [ActionName("Details")]
+        public IActionResult List() => View("Result", new Result { Controller = nameof(HomeController), Action = nameof(List) });
+    }
+}
